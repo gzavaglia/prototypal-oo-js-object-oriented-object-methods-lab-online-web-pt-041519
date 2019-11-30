f0563bda5989ab4be182717d56e8d64047a58313
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-11-30 21:28:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function BoardMember(name, homeState, training){
 BoardMember.prototype.veto = function(){
   return "No, I must disagree"
 }
+
+BoardMember.prototype.approve = function(){
+  return "You can do that!"
+}
+
+BoardMember.prototype.doCharity = function(){
+  return "You can do that!"
+}
